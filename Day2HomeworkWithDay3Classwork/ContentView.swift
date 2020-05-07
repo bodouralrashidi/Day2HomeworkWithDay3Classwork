47e3c5e5ec052064f12ae7488542955a47cb6427
bodour Alrashidi <bodouralrashidi@gmail.com>
2020-05-07 17:58:56 +0300

    Initial Commit

---
+++ Day2HomeworkWithDay3Classwork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day2HomeworkWithDay3Classwork
+//
+//  Created by Bodour Alrashidi on 5/7/20.
+//  Copyright © 2020 Bodour Alrashidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
